9257f1ee20bf6c9fb0ee8b7a4a6fbcc27a592253
Henry Cordes <h.cordes@gmail.com>
2012-02-22 22:15:25 +0100

    Adding solution and starting projects

---
+++ HC.TripData.Domain/Trip.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using DreamSongs.MongoRepository;
+
+namespace HC.TripData.Domain
+{
+    public class Trip: Entity
+    {
+        public int StartMilage { get; set; }
+        public int EndMilage { get; set; }
+        public DateTime DateTime  { get; set; }
+        public string PlaceOfDeparture { get; set; }
+        public string Destination { get; set; }
+        public string DepartureZipCode { get; set; }
+        public string DestinationZipCode { get; set; }
+        public string Description { get; set; }
+        public TripType TripType { get; set; }
+
+        public Driver Driver { get; set; }
+        public Car Car { get; set; }
+    }
+
+    public enum TripType
+    {
+        Business,
+        Private
+    }
+
+    public class Driver
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public List<Car> Cars { get; set; }
+    }
+
+
+    public class Car
+    {
+        public string LicensePlateNumber { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public bool IsCurrentCar { get; set; }
+    }
+}
---
+++ start mongodb.bat
@@ -0,0 +1,5 @@
+D:
+cd Tools
+cd MongoDb
+cd bin
+mongod --dbpath D:\Tools\MongoDb\data\db
